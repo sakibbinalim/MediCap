119e8b145efbc50196c664472dc9867e13443b5c
Sakib Bin Alim <sakibbinalim@gmail.com>
2024-03-06 23:49:37 +0600

    Designing the TextFields

--- MediCap/ContentView.swift
+++ MediCap/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextFieldView(placeholder: "Email", text: $email)
+            Spacer()
+                .frame(height: 20)
+            TextFieldView(placeholder: "Password", text: $password)
         }
-        .padding()
+        .padding(.horizontal, 20.0)
+        
+    }
+}
+
+struct TextFieldView: View {
+    var placeholder: String
+    @Binding var text: String
+    
+    var body: some View {
+        TextField(placeholder, text: $text)
+            .padding(EdgeInsets(top: 22, leading: 20, bottom: 22, trailing: 20))
+            .background(.gray.opacity(0.1))
+            .cornerRadius(10)
     }
 }
 
