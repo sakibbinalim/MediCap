c9016c21a286ceac2e59fa101da9cc7e33627ba4
Sakib Bin Alim <sakibbinalim@gmail.com>
2024-03-06 22:14:30 +0600

    Initial Commit

---
+++ MediCap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediCap
+//
+//  Created by Sakib Bin Alim on 3/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MediCap/MediCapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediCapApp.swift
+//  MediCap
+//
+//  Created by Sakib Bin Alim on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MediCapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
